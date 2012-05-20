51652289ba55a3172559e5ca4afe0784b26f1b20
Seth Johnson <seth.johnson@gmail.com>
2012-05-20 14:20:34 -0700

    make schema more human readable than the dump output

--- database/minibug_schema.sql
+++ database/minibug_schema.sql
@@ -1,61 +1,21 @@
--- MySQL dump 10.13  Distrib 5.1.60, for redhat-linux-gnu (x86_64)
---
--- Host: localhost    Database: minibug
--- ------------------------------------------------------
--- Server version	5.1.60
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS `bug_status_history`;
+DROP TABLE IF EXISTS `bugs`;
+DROP TABLE IF EXISTS `bug_status`;
 
 --
 -- Table structure for table `bug_status`
 --
 
-DROP TABLE IF EXISTS `bug_status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bug_status` (
   `status` varchar(64) NOT NULL,
   PRIMARY KEY (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `bug_status_history`
---
-
-DROP TABLE IF EXISTS `bug_status_history`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bug_status_history` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `bug_id` int(11) NOT NULL,
-  `new_status` varchar(64) NOT NULL,
-  `changed` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `new_status` (`new_status`),
-  KEY `bug_id` (`bug_id`),
-  KEY `changed` (`changed`),
-  CONSTRAINT `bug_status_history_ibfk_1` FOREIGN KEY (`new_status`) REFERENCES `bug_status` (`status`) ON UPDATE CASCADE,
-  CONSTRAINT `bug_status_history_ibfk_2` FOREIGN KEY (`bug_id`) REFERENCES `bugs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `bugs`
 --
 
 DROP TABLE IF EXISTS `bugs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bugs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(256) NOT NULL,
@@ -68,45 +28,35 @@ CREATE TABLE `bugs` (
   KEY `created` (`created`),
   CONSTRAINT `bugs_ibfk_1` FOREIGN KEY (`status`) REFERENCES `bug_status` (`status`) ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = latin1 */ ;
-/*!50003 SET character_set_results = latin1 */ ;
-/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = '' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`sjohnson`@`localhost`*/ /*!50003 TRIGGER update_bug_status_hist_ins AFTER INSERT ON bugs FOR EACH ROW INSERT INTO bug_status_history (bug_id,new_status) VALUES (NEW.id, NEW.status) */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = latin1 */ ;
-/*!50003 SET character_set_results = latin1 */ ;
-/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = '' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 trigger update_bug_status_hist_upd BEFORE UPDATE ON bugs FOR EACH ROW BEGIN IF OLD.status != NEW.status THEN INSERT INTO bug_status_history (bug_id,new_status) VALUES (NEW.id, NEW.status); END IF; END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Table structure for table `bug_status_history`
+--
 
--- Dump completed on 2012-05-20 14:04:12
+DROP TABLE IF EXISTS `bug_status_history`;
+CREATE TABLE `bug_status_history` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bug_id` int(11) NOT NULL,
+  `new_status` varchar(64) NOT NULL,
+  `changed` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `new_status` (`new_status`),
+  KEY `bug_id` (`bug_id`),
+  KEY `changed` (`changed`),
+  CONSTRAINT `bug_status_history_ibfk_1` FOREIGN KEY (`new_status`) REFERENCES `bug_status` (`status`) ON UPDATE CASCADE,
+  CONSTRAINT `bug_status_history_ibfk_2` FOREIGN KEY (`bug_id`) REFERENCES `bugs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=latin1;
+
+DELIMITER $$
+CREATE TRIGGER update_bug_status_hist_ins AFTER INSERT ON bugs 
+FOR EACH ROW 
+	INSERT INTO bug_status_history (bug_id,new_status) VALUES (NEW.id, NEW.status); $$
+
+CREATE TRIGGER update_bug_status_hist_upd BEFORE UPDATE ON bugs 
+FOR EACH ROW 
+BEGIN 
+	IF OLD.status != NEW.status THEN 
+		INSERT INTO bug_status_history (bug_id,new_status) VALUES (NEW.id, NEW.status);
+	END IF;
+END $$
+DELIMITER ;
